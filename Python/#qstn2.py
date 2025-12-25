10962826a149a5357e0891b0f6c852b4c3419dbf
Harini1108 <harinivanathi@gmail.com>
2025-12-25 19:11:12 +0530

    Add files via upload

---
+++ Python/#qstn2.py
@@ -0,0 +1,9 @@
+#asking birth year from user
+birth_year = input("Enter your birth year: ")
+
+#converting it into integer
+birth_year = int(birth_year)
+
+#finding age in 2025
+age = 2025 - birth_year
+print("You will be", age , "years old in 2025!")
