94576f29758a6b35fecd2ead249652ca99e9e214
Harini1108 <harinivanathi@gmail.com>
2025-12-25 18:32:05 +0530

    Add files via upload

---
+++ Python/#Creating variables_1.py
@@ -0,0 +1,10 @@
+#Creating variables of different data types
+name = input("Enter your name:")
+height = float(input("Enter your height:"))
+age = int(input("Enter your age:"))
+student = bool(input("Are you a student(yes/no):"))
+
+print("Name:", name)
+print("Height:", height, "cm")
+print("Age:", age)
+print("Student:", student)
