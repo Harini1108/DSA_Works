b157a1bd8a4b43135433e51c6b2339c3147bd806
Harini1108 <harinivanathi@gmail.com>
2025-12-26 21:43:05 +0530

    Add files via upload

---
+++ Python/#qstn8.py
@@ -0,0 +1,17 @@
+#Creating simple traffic light simulator
+color = input("Enter the traffic light color(red,yellow,green):").lower()
+
+if color == "red":
+ print("STOP")
+
+elif color == "yellow":
+ print("Ready to stop")
+
+elif color == "green":
+ print("You can go")
+
+else:
+ print("Wrong Input!")
+ 
+
+
