7ec88cbaf890f0e1432e991d17bfbfcc3dc0be87
Harini1108 <harinivanathi@gmail.com>
2025-12-26 10:11:39 +0530

    Add files via upload

---
+++ Python/#qstn4.py
@@ -0,0 +1,8 @@
+#Creating variables
+fav_num = int(input("Enter your favourite Number"))
+
+#finding whether the number is odd or even
+if fav_num % 2 == 0:
+ print(fav_num, "is even")
+else:
+ print(fav_num,"is odd")
