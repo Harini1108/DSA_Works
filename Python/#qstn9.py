84d16bbf6639015256abe04bd7b5cc6ea45073d3
Harini1108 <harinivanathi@gmail.com>
2025-12-26 21:54:02 +0530

    Add files via upload

---
+++ Python/#qstn9.py
@@ -0,0 +1,9 @@
+#Asking user for two exam scores
+exam_score_1 = int(input("Enter the score_1:"))
+exam_score_2 = int(input("Enter the score_2:"))
+
+if exam_score_1 >=50 and exam_score_2 >=50:
+ print("you have passed the exam")
+
+else:
+ print("You need to retake some exams")
