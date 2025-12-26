ab09b526c681bc11522ce372655476e16d82349c
Harini1108 <harinivanathi@gmail.com>
2025-12-26 21:05:19 +0530

    Add files via upload

---
+++ Python/#qstn7.py
@@ -0,0 +1,9 @@
+#Asking user a number
+x = int(input("Enter a Number:"))
+
+if x > 0:
+ print("The number is positive")
+elif x < 0:
+ print("The number is negative")
+else:
+ print("The number is zero")
